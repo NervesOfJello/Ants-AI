a4f0829d62f670a4b94a6f5253d46ca130790aed
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-12-04 17:53:12 -0600

    Doing Good
    navmesh, obstacle objects, etc

---
+++ Assets/Scripts/AddObstacleOnClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AddObstacleOnClick : MonoBehaviour {
+
+    //TODO: Handle Movement Differently than Point-And-Click
+    [SerializeField]
+    private Camera cam; //reference the camera component
+
+    [SerializeField]
+    private NavMeshObstacle obstaclePrefab;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(obstaclePrefab, hit.transform);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AntController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AntController : MonoBehaviour {
+
+    //TODO: Handle Movement Differently than Point-And-Click
+    [SerializeField]
+    private Camera cam; //reference the camera component
+    
+    private NavMeshAgent agent; //referencing the nav mesh agent component
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>(); //initialize nave mesh agent component
+    }
+
+    // Update is called once per frame
+    void Update () 
+	{
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
